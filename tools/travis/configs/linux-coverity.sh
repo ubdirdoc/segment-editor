13a1fcc0491d1654b212da00b943189b614678db
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2019-07-16 19:26:37 +0200

    travis

--- tools/travis/configs/linux-coverity.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-source "$CONFIG_FOLDER/linux-source-qt.sh"
-
-if [[ "$TRAVIS_BRANCH" = "$COVERITY_SCAN_BRANCH_PATTERN" ]];
-then
-    $CMAKE_BIN -DBOOST_ROOT="$BOOST_ROOT" -DSCORE_COTIRE:Bool=False -DSCORE_CONFIGURATION=debug ..
-
-    eval "$COVERITY_SCAN_BUILD"
-fi
