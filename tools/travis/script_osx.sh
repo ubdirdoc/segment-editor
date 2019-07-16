6d4c1f0b30074a5ed645b5f3d18e5bfe7895cb2d
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2019-07-16 19:20:16 +0200

    travis

--- tools/travis/script_osx.sh
+++ tools/travis/script_osx.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-cmake -DSEGMENT_RELEASE=1 -DDEPLOYMENT_BUILD=1 -DCMAKE_INSTALL_PREFIX=appdir/usr -DCMAKE_SKIP_RPATH=ON
+cmake -DSEGMENT_RELEASE=1 -DDEPLOYMENT_BUILD=1 -DCMAKE_INSTALL_PREFIX=inst -DCMAKE_SKIP_RPATH=ON
 
 cmake --build . --target all_unity -- -j$(nproc)
 cmake --build . --target docs
 cmake --build . --target install/strip/fast
 
-cd 
+find inst/
