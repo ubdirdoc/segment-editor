d2259d45f3a0680b295eebf9c01aca972d4eccd1
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2019-07-16 23:20:50 +0200

    release test 2

--- tools/travis/before_install_macos.sh
+++ tools/travis/before_install_macos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-brew install boost
-pip install sphinx quark-sphinx-theme gnu-tar wget
+brew install gnu-tar wget
+pip install sphinx quark-sphinx-theme
 wget https://github.com/ubdirdoc/segment-editor/releases/download/sdk/qt-5.9.7-macos.tar.gz 
 gtar xaf qt-5.9.7-macos.tar.gz 
